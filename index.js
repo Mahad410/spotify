30ba2f810db50e2c0ed23219aa39fa53306d8993
Mahad410 <mahadqureshi410@gmail.com>
2022-02-04 16:14:09 -0600

    spotify

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const ham = document.getElementById('ham');
+const overlay = document.querySelector('.overlay');
+const sidenav = document.querySelector('.sidenav');
+if (window.innerWidth <= '900') {
+    ham.style.display = 'block';
+} else {
+    ham.style.display = 'none';
+}
 window.onresize = () => {
     if (window.innerWidth <= '900') {
         ham.style.display = 'block';
     } else {
         ham.style.display = 'none';
     }
-    console.log(window.innerWidth);
 }
+
+function toggle() {
+    overlay.classList.toggle('show_or');
+    sidenav.classList.toggle('show_s');
+}
+
+ham.addEventListener('click', toggle);
+overlay.addEventListener('click', toggle);
