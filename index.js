7057806cf159c17cded161da67253132d29318a9
Mahad410 <mahadqureshi410@gmail.com>
2022-02-04 18:19:33 -0600

    spotify

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const ham = document.getElementById('ham');
 const overlay = document.querySelector('.overlay');
 const sidenav = document.querySelector('.sidenav');
+const sidemenu = document.querySelector('.sidemenu');
+const x_btn = document.querySelector('.x_b');
 if (window.innerWidth <= '900') {
     ham.style.display = 'block';
+    x_btn.style.display = 'block';
+    sidelink.style.display = 'block';
+    sidemenu.style.display = 'block';
 } else {
     ham.style.display = 'none';
+    x_btn.style.display = 'none';
+    sidemenu.style.display = 'none';
+    overlay.style.display = 'none';
 }
 window.onresize = () => {
     if (window.innerWidth <= '900') {
         ham.style.display = 'block';
+        x_btn.style.display = 'block';
+        sidemenu.style.display = 'block';
     } else {
         ham.style.display = 'none';
+        x_btn.style.display = 'none';
+        sidemenu.style.display = 'none';
+        overlay.style.display = 'none';
     }
 }
 
@@ -21,3 +34,4 @@ function toggle() {
 
 ham.addEventListener('click', toggle);
 overlay.addEventListener('click', toggle);
+x_btn.addEventListener('click', toggle);
